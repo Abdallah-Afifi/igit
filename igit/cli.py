5b6f0beddba466ea9019557d60f8be9857c8790b
Abdallah Afifi <abdallahfathi@aucegypt.edu>
2025-03-14 02:31:40 -0400

    adding reset command to cli

--- igit/cli.py
+++ igit/cli.py
@@ -68,6 +68,10 @@ def parse_args():
     status_parser = commands.add_parser('status')
     status_parser.set_defaults(func=status)
 
+    reset_parser = commands.add_parser('reset')
+    reset_parser.set_defaults(func=base.reset)
+    reset_parser.add_argument('commit', type=oid)
+
 
 
     return parser.parse_args()
@@ -155,3 +159,5 @@ def status(args):
     else:
         print('HEAD detached at', HEAD[:10])
 
+def reset(args):
+    base.reset(args.commit)
