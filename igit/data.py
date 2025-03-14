9f1cdc536d9d5b212e3165b6b11cadce526a96d7
Abdallah Afifi <abdallahfathi@aucegypt.edu>
2025-03-14 13:40:13 -0400

    function to push an object from local to remote

--- igit/data.py
+++ igit/data.py
@@ -94,6 +94,10 @@ def fetch_object_if_missing(oid, remote_git_dir):
         return
     remote_git_dir += '/.igit'
     shutil.copy(f'{remote_git_dir}/objects/{oid}', f'{GIT_DIR}/objects/{oid}')
+
+def push_object(oid, remote_git_dir):
+    remote_git_dir += '/.igit'
+    shutil.copy(f'{GIT_DIR}/objects/{oid}', f'{remote_git_dir}/objects/{oid}')
     
 
 
