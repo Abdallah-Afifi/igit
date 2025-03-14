db2944b7b74e60d109e0cd97126f03f47d9996d3
Abdallah Afifi <abdallahfathi@aucegypt.edu>
2025-03-14 13:04:54 -0400

    function to change current igit dir

--- igit/data.py
+++ igit/data.py
@@ -2,8 +2,18 @@ import hashlib
 import os 
 
 from collections import namedtuple
+from contextlib import contextmanager
 
-GIT_DIR = '.igit'
+# initialized in cli.main()
+GIT_DIR = None
+
+@contextmanager
+def change_git_dir(new_dir):
+    global GIT_DIR
+    old_dir = GIT_DIR
+    GIT_DIR = f'{new_dir}/.igit'
+    yield
+    GIT_DIR = old_dir
 
 def init ():
     os.makedirs(GIT_DIR)
