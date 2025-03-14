3f54fdbb879a89d30499ee2d0b183e7fdf8bba83
Abdallah Afifi <abdallahfathi@aucegypt.edu>
2025-03-14 01:49:42 -0400

    refactoring to use ref.value

--- igit/base.py
+++ igit/base.py
@@ -78,7 +78,7 @@ def read_tree(tree_oid):
 def commit(message):
     commit = f'tree {write_tree()}\n'
 
-    HEAD = data.get_ref()
+    HEAD = data.get_ref().value
     if HEAD:
         commit += f'parent {HEAD}\n'
 
@@ -86,19 +86,19 @@ def commit(message):
     commit += f'{message}\n'
 
     oid = data.hash_object(commit.encode(), 'commit')
-    data.update_ref('HEAD', oid)
+    data.update_ref('HEAD', data.RefValue(symbolic=False, value=oid))
     return oid
 
 def checkout(oid):
     commit = get_commit(oid)
     read_tree(commit.tree)
-    data.update_ref(oid)
+    data.update_ref('HEAD', data.RefValue(symbolic=False, value=oid))
 
 def create_tag(name, oid):
-    data.update_ref(f'refs/tags/{name}', oid)
+    data.update_ref(f'refs/tags/{name}', data.RefValue(symbolic=False, value=oid))
 
 def create_branch(name, oid):
-    data.update_ref(f'refs/heads/{name}', oid)
+    data.update_ref(f'refs/heads/{name}', data.RefValue(symbolic=False, value=oid))
 
 Commit = namedtuple('commit', ['tree', 'parent', 'message'])
 
@@ -142,8 +142,8 @@ def get_oid(name):
         f'refs/heads/{name}',
     ]
     for ref in refs_to_try:
-        if data.get_ref(ref):
-            return data.get_ref(ref)
+        if data.get_ref(ref).value:
+            return data.get_ref(ref).value
     # hash
     is_hex = all(c in string.hexdigits for c in name)
     if len(name) == 40 and is_hex:
