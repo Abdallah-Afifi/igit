4530b1b71318362de262195389666a275dabbc70
Abdallah Afifi <abdallahfathi@aucegypt.edu>
2025-03-14 13:11:07 -0400

    function to fetch a list of remote refs

---
+++ igit/remote.py
@@ -0,0 +1,7 @@
+from . import data
+
+def fetch(remote_path):
+    print('will fetch the following refs: ')
+    with data.change_git_dir(remote_path):
+        for refname, _ in data.iter_refs('refs/heads'):
+            print(f'- {refname}')
