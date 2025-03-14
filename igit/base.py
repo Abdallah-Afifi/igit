0f9213e118446dfd142e3e4c41045bdb47e7d57b
Abdallah Afifi <abdallahfathi@aucegypt.edu>
2025-03-14 14:10:48 -0400

    chnaging write_tree to write from index

--- igit/base.py
+++ igit/base.py
@@ -12,26 +12,34 @@ def init():
     data.init()
     data.update_ref('HEAD', data.RefValue(symbolic=True, value='refs/heads/master'))
 
-def write_tree(directory='.'):
-    entries = []
-    with os.scandir(directory) as it:
-        for entry in it:
-            full = f'{directory}/{entry.name}'
-
-            if is_ignored(full):
-                continue    
-
-            if entry.is_file(follow_symlinks=False):
-                type_ = 'blob'
-                with open(full, 'rb') as f:
-                    oid = data.hash_object(f.read())
-            elif entry.is_dir(follow_symlinks=False):
+def write_tree():
+    # conv index to tree of dicts
+    index_as_tree = {}
+    with data.get_index() as index:
+        for path, oid in index.items():
+            path = path.split('/')
+            dirpath, filename = path[:-1], path[-1]
+            
+            current = index_as_tree
+            # find dict of dir
+            for dirname in dirpath:
+                current = current.setdefault(dirname, {})
+            current[filename] = oid
+    def write_tree_recursive(tree_dict):
+        entries = []
+        for name, value in tree_dict.items():
+            if type(value) is dict:
                 type_ = 'tree'
-                oid = write_tree(full)
-            entries.append((entry.name, oid, type_))
-    # TODO write the tree object
-    tree = ''.join(f'{type_} {oid} {name}\n' for name, oid, type_ in sorted(entries))
-    return data.hash_object(tree.encode(), 'tree')
+                oid = write_tree_recursive(value)
+            else:
+                type_ = 'blob'
+                oid = value
+            entries.append((name, oid, type_))
+        tree = ''.join(f'{type_} {oid} {name}\n' for name, oid, type_ in sorted(entries))
+        return data.hash_object(tree.encode(), 'tree')
+    
+    return write_tree_recursive(index_as_tree)
+
 
 def _iter_tree_entries(oid):
     if not oid:
