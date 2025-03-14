860ba2cdaff6c01d2a9a52ce0fe7d563372a0ef1
Abdallah Afifi <abdallahfathi@aucegypt.edu>
2025-03-14 13:42:44 -0400

    function to push objects from local to remote

--- igit/remote.py
+++ igit/remote.py
@@ -19,6 +19,21 @@ def fetch(remote_path):
         refname = os.path.relpath(remote_name, REMOTE_REFS_BASE)
         data.update_ref(f'{LOCAL_REFS_BASE}/{refname}', data.RefValue(symbolic=False, value=value))
 
+def push(remote_path, refname):
+    # get refs data
+    local_ref = data.get_ref(refname).value
+    assert local_ref
+
+    objects_to_push = base.iter_objects_in_commits({local_ref})
+
+    # push all objects
+    for oid in objects_to_push:
+        data.push_object(oid, remote_path)
+    
+    # update server ref to our value
+    with data.change_git_dir(remote_path):
+        data.update_ref(refname, data.RefValue(symbolic=False, value=local_ref))
+
 def _get_remote_refs(remote_path, prefix=''):
     with data.change_git_dir(remote_path):
         return {refname: ref.value for refname, ref in data.iter_refs(prefix)}
