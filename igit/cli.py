e4206e16c606282fc9b554c02de42a6095e66ca4
Abdallah Afifi <abdallahfathi@aucegypt.edu>
2025-03-14 02:28:07 -0400

    more covenient log with refs to commit list

--- igit/cli.py
+++ igit/cli.py
@@ -97,9 +97,13 @@ def commit(args):
     print (base.commit(args.message))
 
 def log(args):
+    refs = {}
+    for refname, ref in data.iter_refs():
+        refs.setdefault(ref.value, []).append(refname)
     for oid in base.iter_commits_and_parents({args.oid}):
         commit = base.get_commit(oid)
-        print(f'commit {oid}\n')
+        refs_str = f' ({", ".join(refs[oid])})' if oid in refs else ''
+        print(f'commit {oid}{refs_str}\n')
         print(textwrap.indent(commit.message, '    '))
         print('')
 
@@ -119,7 +123,7 @@ def branch(args):
         base.create_branch(args.name, args.start_point)
         print (f'Branch {args.name} created at {args.start_point[:10]}')
 
-        
+
 def k(args):
     dot = 'digraph commits {\n'
     oids = set()
